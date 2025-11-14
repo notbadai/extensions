67e182dfec063866cfabc41615dd41104ff25d91
hnipun <hnipun@gmail.com>
2025-11-14 09:40:10 +0530

    new version

--- tools_chat/setup.py
+++ tools_chat/setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="notbadai_tools_chat",
-    version="0.1.1",
+    version="0.1.2",
     author="NotBadAI Team",
     author_email="contact@notbad.ai",
     description="An intelligent programming assistant powered by AI",
